c6383b618ed2d62c8f4ce21265daf7ee9d5544bc
Aleksandra Korach <sgsbox90@gmail.com>
2015-09-05 14:36:20 +0200

    Renamed and optimised (used peek instead of poll), removed redundant code. Please test.

--- handin_02/IP.java
+++ handin_02/IP.java
@@ -1,58 +1,45 @@
 import java.util.*;
 
 public class IP {
-
-    private static boolean debug = false;
-
+    private static final boolean debug = false;
     private static int n;
     private static Job[] jobs;
-    private static PriorityQueue<Priority> priority;
-    private static int priorityCapacity = 10; // remove
-
+    private static PriorityQueue<Resource> resource;
     private static int cur = 0;
     
     public static void main(String[] args) {
 	parseInput();
-	priority = new PriorityQueue<Priority>(n, (a, b) -> a.getFree() - b.getFree());	
+	resource = new PriorityQueue<>(n, (a, b) -> a.getFree() - b.getFree());	
 	Arrays.sort(jobs, (a, b) -> a.getStartTime() - b.getStartTime());
 
-	// TODO: Change label / index name
-	
-	//	while(true) {
-	    for(int i = 0; i < jobs.length; i++) {
-		if(priority.size() == 0) {
-		    Priority p = new Priority(cur++);
-		    p.setFree(jobs[i].getEndTime());
-		    jobs[i].setLabel(p.getIndex());
-		    priority.add(p);
-		} else {
-		    Priority p = priority.poll();
-		    boolean overlap = isOverlap(jobs[i], p.getFree());
+        for (Job job : jobs) {
+            if (resource.size() == 0) {
+                Resource p = new Resource(cur++);
+                p.setFree(job.getEndTime());
+                job.setLabel(p.getLabel());
+                resource.add(p);
+            } else {
+                Resource p = resource.peek();
+                boolean overlap = isOverlap(job, p.getFree());
+                if (overlap) {
+                    Resource p2 = new Resource(cur++);
+                    p2.setFree(job.getEndTime());
+                    job.setLabel(p2.getLabel());
+                    resource.add(p2);
+                } else {
+                    p.setFree(job.getEndTime());
+                    job.setLabel(p.getLabel());
+                }
+            }
+        }
+	//System.out.println(cur + "\n");
 
-		    if(overlap) {
-			Priority p2 = new Priority(cur++);
-			p2.setFree(jobs[i].getEndTime());
-			jobs[i].setLabel(p2.getIndex());
-			priority.add(p2);
-			priority.add(p);
-		    } else {
-			p.setFree(jobs[i].getEndTime());
-			jobs[i].setLabel(p.getIndex());
-			priority.add(p);
-		    }
-		}
-	    }
-	    //	}
-
-	    System.out.println(cur + "\n");
-
-	    for(Job j : jobs)
-		System.out.println(j.toString());
+	for(Job j : jobs)
+            System.out.println(j.toString());
     }
 
     public static boolean isOverlap(Job j, int et) {
-	if(j.getStartTime() < et) return true;
-	else return false;
+        return j.getStartTime() < et;
     }
 
     public static void parseInput() {
@@ -63,17 +50,14 @@ public class IP {
 	jobs = new Job[n];
 
 	int cur = 0;
-
 	while(in.hasNextLine()) {
 	    String l = in.nextLine();
 	    if(l == null || l.length() == 0) continue;
 	    String[] input = l.split(" ");
 
 	    jobs[cur] = new Job(Integer.parseInt(input[0]), Integer.parseInt(input[1]));
-	    if(cur < jobs.length)
-		cur++;
-	    else
-		break;
+	    if(cur < jobs.length) cur++;
+	    else break;
 	}
 
 	if(debug)
@@ -86,8 +70,8 @@ public class IP {
     }
 
     public static class Job {
-	private int startTime;
-	private int endTime;
+	private final int startTime;
+	private final int endTime;
 	private int label;
 
 	public Job(int startTime, int endTime) {
@@ -95,34 +79,28 @@ public class IP {
 	    this.endTime = endTime;
 	    this.label = Integer.MIN_VALUE;
 	}
-
-	public int getLabel() { return label; }
+        
 	public void setLabel(int label) { this.label = label; }
-
 	public int getStartTime() { return startTime; }
-	public void setStartTime(int startTime) { this.startTime = startTime; }
-
 	public int getEndTime() { return endTime; }
-	public void setEndTime(int endTime) { this.endTime = endTime; }
 
+        @Override
 	public String toString() {
 	    //	    return String.format("Start time: %d, End time: %d, label: %d", startTime, endTime, label);
 	    return String.format("%d %d %d", startTime, endTime, label);	    
 	}
     }
 
-    public static class Priority {
-	private int index;
+    public static class Resource {
+	private final int label;
 	private int free;
 
-	public Priority(int index) {
-	    this.index = index;
+	public Resource(int label) {
+	    this.label = label;
 	    this.free = Integer.MIN_VALUE;
 	}
 
-	public int getIndex() { return index; }
-	public void setIndex(int index) { this.index = index; }
-
+	public int getLabel() { return label; }        
 	public int getFree() { return free; }
 	public void setFree(int free) { this.free = free; }
     }
