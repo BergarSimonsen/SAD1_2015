a5da0f2b334ee5f6ab9fe1443a31d7c3c89aebf2
Bergar Simonsen <bergar.simonsen@gmail.com>
2015-09-09 10:46:15 +0200

    removed closest pair

--- handin_03/CP.java
+++ handin_03/CP.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class CP {
 
@@ -11,11 +12,23 @@ public class CP {
     private static Pattern patternNumber = Pattern.compile(pairNumber);
     private static Pattern patternName = Pattern.compile(pairName);
 
-    private static int matches = 0;
-    private static int lines = 0;
+    private static int n = 0;
+    private static double P[] = new double[10];    
     
     public static void main(String[] args) {
 	parseInput();
+
+        Arrays.sort(P);
+        
+        double Px[] = new double[n/2];
+        double Py[] = new double[n/2];
+        
+        Point result = ClosestPairRec(Px, Py);	
+    }
+
+    private static Point ClosestPairRec(double[] px, double[] py) {
+	
+        return null;
     }
 
     public static void parseInput() {
@@ -25,10 +38,7 @@ public class CP {
 	    if(!in.hasNextLine()) break;
 
 	    String l = in.nextLine();
-	    lines++;
 	    
-	    System.out.println(l);
-
 	    Matcher matcherNumber = patternNumber.matcher(l);
 	    Matcher matcherName = patternName.matcher(l);
 
@@ -38,42 +48,43 @@ public class CP {
 		parseNamePoint(l);
 	    }
 	}
-
-	System.out.println(String.format("Lines: %d, matches: %d", lines, matches));
     }
 
     public static void parseNamePoint(String l) {
-	System.out.println("ParseNamePoint: " + l);
-	matches++;
+	//	System.out.println("ParseNamePoint: " + l);
+	System.out.println("----------------------------------------");
+	String[] tmp = l.split(" ");
+	System.out.println("Line: " + l);
+	for(int i = 0; i < tmp.length; i++)
+	    System.out.println(String.format("tmp[%d] = %s", i, tmp[i]));
+	System.out.println("----------------------------------------");	
+	n++;
     }
 
     public static void parseNumberPoint(String l) {
-	System.out.println("ParseNumberPoint: " + l);
-	matches++;
+	//	System.out.println("ParseNumberPoint: " + l);
+	String[] tmp = l.split(" ");
+	System.out.println("----------------------------------------");
+	System.out.println("Line: " + l);
+	for(int i = 0; i < tmp.length; i++)
+	    System.out.println(String.format("tmp[%d] = %s", i, tmp[i]));
+	System.out.println("----------------------------------------");	
+	n++;
     }
 }
 
-/*
-	String p1 = "\\d* .*";
-	String p2 = "\\d*(:) .*";
-	Pattern r = Pattern.compile(p1);
-	Pattern r2 = Pattern.compile(p2);
-	
-	// p1 and r are regex patterns for finding the entities, i.e. 1 Sheldon
-	// p2 and r2 are regex patterns for finding the preferences, i.e. 1: 2 4 6 8
-
-	while(true) {
-	    if(!in.hasNextLine()) break;
-	    String l = in.nextLine();
-	    if(l == null) continue;
-	    Matcher m = r.matcher(l);
-	    Matcher m1 = r2.matcher(l);
-	    if(l.startsWith("#")) continue;
-	    else if(l.startsWith("n")) {
-		sm.setN(sm.parseN(l));
-		sm.setA(new Entity[sm.getN()]);
-		sm.setB(new Entity[sm.getN()]);
-		sm.setMatch(new Pair[sm.getN()]);
-	    } else if(m.matches()) {
-		parseEntities(l);
- */
+class Point {    
+    private final double x;
+    private final double y;
+    private String name;
+    
+    public Point(double x, double y, String name) {
+        this.x = x;
+        this.y = y;
+        this.name = name;
+    }
+    
+    public double getX() { return x; }
+    public double getY() { return y; }
+    public String getName() { return name; }
+}
