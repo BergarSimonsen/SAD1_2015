ffc91ce1e90677a78005354bbb8ea4306a517aeb
Aleksandra Korach <sgsbox90@gmail.com>
2015-09-12 18:42:53 +0200

    Algorithm written, contains bugs.

--- handin_03/CP.java
+++ handin_03/CP.java
@@ -1,5 +1,7 @@
-import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -14,7 +16,7 @@ public class CP {
     */
     
     private static final String pairNumber = "(\\s)*-?[0-9]+(\\s)*-?[0-9]+(\\s)*-?[0-9]+";
-    private static final String pairName = "[a-zA-Z]+ -?[0-9]+ -?[0-9]+";
+    private static final String pairName = "[a-zA-Z]+ -?[0-9]+ -?[0-9]+";    
     private static final String dimension = "DIMENSION: [0-9]+";
     private static final String pairScientific = "(\\s)*-?[0-9]+(\\s)*-?[0-9]+-?[0-9]*[.][0-9]*[e][+][0-9]+(\\s)*-?[0-9]*[.][0-9]*[e][+][0-9]+";
 
@@ -27,27 +29,100 @@ public class CP {
     private static Point P[];
     private static int curIndex = 0;
     
-    
     public static void main(String[] args) {
         parseInput();
-
-	if(P == null) die();
-
-	printPointArray(P);
+        if(P == null) die();
+        //printPointArray(P);
         
-	/*        Point Px[] = new Point[n];        
+        Point Px[] = new Point[n];        
         Point Py[] = new Point[n];        
         System.arraycopy(P, 0, Px, 0, P.length);
         System.arraycopy(P, 0, Py, 0, P.length);
-        Arrays.sort(Px, (a, b) -> (int) (a.getX() - b.getY()));
-        Arrays.sort(Px, (a, b) -> (int) (a.getX() - b.getY())); */
+        System.out.println("After creating Px & Py");
+        Arrays.sort(Px, new PointCmpX());
+        Arrays.sort(Py, new PointCmpY());
         
-	//        Point result = closestPairRec(Px, Py);
+        Point[] result = closestPairRec(Px, Py);
+        System.out.println(n + " " + getDistance(result[0], result[1]));
+    }    
+    
+    private static Point[] closestPairRec(Point[] px, Point[] py) {
+        if (px.length <= 3){
+            Point[] pair = null;
+            double d = Double.MAX_VALUE;
+            for(int i = 0 ; i < px.length ; i++)
+                for(int j = i + 1 ; j < px.length ; j++) {
+                    if (getDistance(px[i], px[j]) < d) {
+                        pair[0] = px[i];
+                        pair[1] = px[j];
+                    }
+                }
+            return pair; 
+        }  
+        
+        Point qx[] = new Point[(int)Math.floor(px.length/2)];
+        Point qy[] = new Point[(int)Math.ceil(px.length/2)];
+        Point rx[] = new Point[(int)Math.floor(px.length/2)];
+        Point ry[] = new Point[(int)Math.ceil(px.length/2)];
+        for(int i = 0 ; i < (int)Math.floor(px.length/2) ; i++) {
+            qx[i] = px[i];
+            qy[i] = py[i];
+        }        
+        for(int i = (int)Math.ceil(px.length/2) ; i < px.length ; i++) {
+            rx[i] = px[i];
+            ry[i] = py[i];
+        }
+        
+        Point pairQ[] = closestPairRec(qx, qy);
+        Point pairR[] = closestPairRec(rx, ry);
+        
+        double delta = Math.min(getDistance(pairQ[0], pairQ[1]),
+                getDistance(pairR[0], pairR[1]));
+        Point l = getMaxXPoint(qx);
+        double maxX = l.getX();
+        
+        List<Point> sy = new ArrayList<>();
+        for (Point p : py) {
+            if (p.getX() <= Math.abs(maxX - delta)) {
+                sy.add(p);
+            }
+        }
+        
+        double d = Double.MAX_VALUE;
+        Point[] pair = new Point[2];
+        for(int i = 0 ; i < sy.size() ; i++)         
+            for(int j = i + 1 ; j < 15 ; j++) {
+                if(j >= sy.size()) break;
+                double pairD = getDistance(sy.get(i), sy.get(j));
+                if(pairD < d) {
+                    d = pairD;
+                    pair[0] = sy.get(i);
+                    pair[1] = sy.get(j);
+                }
+            }
+        
+        if (d < delta)     
+            return pair;
+        else if (getDistance(pairQ[0], pairQ[1]) <= getDistance(pairR[0], pairR[1]))
+                return pairQ;
+        else return pairR;
     }
     
+    private static double getDistance(Point a, Point b) {
+        return Math.sqrt(Math.pow(b.getX() - a.getX(), 2) 
+                + Math.pow(b.getY() - a.getY(), 2));
+    }
     
-    private static Point closestPairRec(Point[] px, Point[] py) {
-        return null;
+    private static Point getMaxXPoint(Point[] qx) {
+        Point pMax = null;
+        double max = Double.MIN_VALUE;
+        for (Point p : qx) {
+            if (p.getX() > max) {
+                pMax = p;
+            }
+        }
+        if(pMax == null) die();
+        return pMax;
     }
     
     public static void parseInput() {
@@ -75,7 +150,7 @@ public class CP {
 	    }
 	}
     }
-
+    
     public static void parseDimension(String l) {
 	String[] tmp = l.split(" ");
 	n = Integer.parseInt(tmp[1]);
@@ -84,7 +159,7 @@ public class CP {
     
     public static void parsePoint(String l) {
 	String[] tmp = l.split(" ");
-	ArrayList<String> list = new ArrayList<String>();
+	List<String> list = new ArrayList<>();
 
 	for(String s : tmp) {
 	    if(s != null && s.length() > 0)
@@ -96,7 +171,7 @@ public class CP {
 	System.out.println(p.toString());
 	P[curIndex++] = p;
     }
-
+    
     private static void printPointArray(Point[] arr) {
 	for(int i = 0; i < arr.length; i++) 
 	    System.out.println(String.format("arr[%d] == %s", i, arr[i].toString()));
@@ -106,13 +181,13 @@ public class CP {
 	// Fatal error. Kill program.
 	System.out.println("Error parsing input. Exiting.");
 	System.exit(-1);
-    }    
+    }      
 }
 
 class Point {    
     private final double x;
     private final double y;
-    private String name;
+    private final String name;
     
     public Point(double x, double y, String name) {
         this.x = x;
@@ -123,9 +198,23 @@ class Point {
     public double getX() { return x; }
     public double getY() { return y; }
     public String getName() { return name; }
-
+    
     @Override
     public String toString() {
 	return String.format("%s (%f, %f)", getName(), getX(), getY());
+    }
+}
+
+class PointCmpX implements Comparator<Point> {
+    @Override
+    public int compare(Point a, Point b) {
+        return Double.compare(a.getX(), b.getX());
+    }
+}
+
+class PointCmpY implements Comparator<Point> {
+    @Override
+    public int compare(Point a, Point b) {
+        return Double.compare(a.getY(), b.getY());
     }
 }
