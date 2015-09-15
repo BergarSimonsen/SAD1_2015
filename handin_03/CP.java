1e373df748a6df4ae71a6e2d56a84f58d54e1d1d
Aleksandra Korach <sgsbox90@gmail.com>
2015-09-15 09:52:54 +0200

    Corrected algorithm.

--- handin_03/CP.java
+++ handin_03/CP.java
@@ -1,5 +1,7 @@
+import java.awt.geom.Point2D;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
@@ -9,7 +11,6 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.awt.geom.Point2D;
 
 public class CP {
     /*
@@ -40,187 +41,107 @@ public class CP {
         parseInput();
         if(P == null) die("Error parsing input. Exiting.");
 
-	//	System.out.println(String.format("Dimension: %d, P.length: %d", n, P.length));
+	System.out.println(String.format("Dimension: %d, P.length: %d", n, P.length));
         
         Point2D.Double Px[] = new Point2D.Double[n];        
         Point2D.Double Py[] = new Point2D.Double[n];        
         System.arraycopy(P, 0, Px, 0, P.length);
         System.arraycopy(P, 0, Py, 0, P.length);
         Arrays.sort(Px, new PointCmpX());
-	Arrays.sort(Py, new PointCmpY()); 
-	//	System.out.println("XX");
-
-	/*	Arrays.sort(Px, (a,b) -> (int) (a.getX() - b.getX()));
-	Arrays.sort(Py, (a,b) -> (int) (a.getY() - b.getY())); */
-
-	/*	printPointArray(P, "P");
-	printPointArray(Px, "Px");
-	printPointArray(Py, "Py "); */
-
+        Arrays.sort(Py, new PointCmpY());
+        
         Point2D.Double[] result = closestPairRec(Px, Py);
-	System.out.println(n + " " + getDistance(result[0], result[1]));
-	//        System.out.println(n + " " + Math.sqrt(getDistance(result[0], result[1])));
+        System.out.println(n + " " + getDistance(result[0], result[1]));
     }    
     
     private static Point2D.Double[] closestPairRec(Point2D.Double[] px, Point2D.Double[] py) {
         if (px.length <= 3){
             Point2D.Double[] pair = new Point2D.Double[2];
-            double d = Double.MAX_VALUE;
-            for(int i = 0 ; i < px.length ; i++) {
+            double d = Double.POSITIVE_INFINITY;
+            for(int i = 0 ; i < px.length ; i++)
                 for(int j = i + 1; j < px.length ; j++) {
                     double pairD = getDistance(px[i], px[j]);
-		    if (Double.compare(pairD, d) < 0) {
+                    if (Double.compare(pairD, d) < 0) {
                         pair[0] = px[i];
                         pair[1] = px[j];
                         d = pairD;
-		    } 
+                    }
                 }
-	    }
             return pair; 
-        }
-
-	int higherHalf = (int) Math.ceil(px.length / 2.0d);
+        } 
+        
+        int higherHalf = (int) Math.ceil(px.length / 2.0d);
 	int lowerHalf = (int) Math.floor(px.length / 2.0d);
-
+        
 	Point2D.Double qx[] = new Point2D.Double[higherHalf];
         Point2D.Double qy[] = new Point2D.Double[higherHalf];
         Point2D.Double rx[] = new Point2D.Double[lowerHalf];
         Point2D.Double ry[] = new Point2D.Double[lowerHalf];
-
-	//	System.out.println(lowerHalf);
-
         for(int i = 0; i < higherHalf; i++) {
             qx[i] = px[i];
-            qy[i] = py[i];
+            qy[i] = px[i];
         }
-
+        Arrays.sort(qy, new PointCmpY());
         for(int i = 0; i < lowerHalf ; i++) {
-	    //	    System.out.println(i + " " + lowerHalf + " " + (i + lowerHalf) + " " + px.length + " " + rx.length);
             rx[i] = px[i + higherHalf];
-            ry[i] = py[i + higherHalf];
+            ry[i] = px[i + higherHalf];
         }
-
-	/*	printPointArray(qx, "qx");
-	printPointArray(qy, "qy");
-	printPointArray(rx, "rx");
-	printPointArray(ry, "ry");	 */
-	
-	/*        Point2D.Double qx[] = new Point2D.Double[(int)Math.floor(px.length/2.0d)];
-        Point2D.Double qy[] = new Point2D.Double[(int)Math.floor(px.length/2.0d)];
-        Point2D.Double rx[] = new Point2D.Double[(int)Math.ceil(px.length/2.0d)];
-        Point2D.Double ry[] = new Point2D.Double[(int)Math.ceil(px.length/2.0d)]; */
-	
-	/*        for(int i = 0 ; i < (int)Math.floor(px.length/2.0d) ; i++) {
-            qx[i] = px[i];
-            qy[i] = py[i];
-        }
-	
-        for(int i = (int)Math.floor(px.length/2.0d) ; i < px.length ; i++) {
-            rx[i - (int)Math.floor(px.length/2.0d)] = px[i];
-            ry[i - (int)Math.floor(px.length/2.0d)] = py[i];
-	    } */
-
+        Arrays.sort(ry, new PointCmpY());
         Point2D.Double pairQ[] = closestPairRec(qx, qy);
         Point2D.Double pairR[] = closestPairRec(rx, ry);
         
-//        double delta = Math.min(getDistance(pairQ[0], pairQ[1]),
-//                getDistance(pairR[0], pairR[1]));
-
-/*	printPointArray(pairQ, "pairQ");
-	printPointArray(pairR, "pairR");	 */
-
-        double delta;
-        double dq = getDistance(pairQ[0], pairQ[1]);
-        double dr = getDistance(pairR[0], pairR[1]);
-	
-        if(Double.compare(dq, dr) <= 0) delta = dq;
-        else delta = dr;
-
-	//	System.out.println("DELTA: " + delta);
-
-	//	System.out.println(String.format("dq: %f, dr %f, delta: %f", dq, dr, delta));
-	
-	//        Point2D.Double l = getMaxXPoint(qx);
-	double l = getMaxXPoint(qx).getX();
-	//	System.out.println("XXXXXXXXXXXXXXXXXXXx " + l);
+        double delta = Math.min(getDistance(pairQ[0], pairQ[1]),
+                getDistance(pairR[0], pairR[1]));
+        double l = getL(qx);
         
         List<Point2D.Double> sy = new ArrayList<>();
         for (Point2D.Double p : py) {
-	    if(Double.compare(l, p.getX()) <= 0) {
+            BigDecimal bl = new BigDecimal(l);
+            BigDecimal bdelta = new BigDecimal(delta);
+            BigDecimal left = bl.subtract(bdelta);
+            BigDecimal right = bl.add(bdelta);
+            if(Double.compare(p.x, left.doubleValue()) >= 0 
+                    && Double.compare(p.x, right.doubleValue()) <= 0) {
 		sy.add(p);
 	    }
-	    /*            if (Double.compare(getDistance(l, p), delta) <= 0) {
-                sy.add(p);
-		} */
         }
-
-	/*	System.out.println("-------------");
-	for(Point2D.Double pd : sy) {
-	    System.out.println(String.format("%f, %f", pd.getX(), pd.getY()));
-	}
-	System.out.println("-------------");  */
-
-	//	System.out.println("------------------- " + sy.size());
-
+        
         double d = Double.POSITIVE_INFINITY;
         Point2D.Double[] pair = new Point2D.Double[2];
-	
-        for(int i = 0 ; i < sy.size() ; i++) {    
-            for(int j = i + 1 ; j < i + 15 ; j++) {
+        for(int i = 0 ; i < sy.size() ; i++)         
+            for(int j = i + 1; j < 16 + i ; j++) {
                 if(j >= sy.size()) break;
                 double pairD = getDistance(sy.get(i), sy.get(j));
-		//System.out.println("PairD: " + pairD);
                 if(Double.compare(pairD, d) < 0) {
                     d = pairD;
-		    //		    System.out.println("d : " + d);
                     pair[0] = sy.get(i);
                     pair[1] = sy.get(j);
                 }
-            }
-	}
-        
+            }        
                
-        if (Double.compare(d, delta) < 0) {
-	    /*	    System.out.println("pair ");
-	    for(Point2D.Double pd : pair)
-	    System.out.println(pd);  */
+        if (Double.compare(d, delta) < 0)     
             return pair;
-        } else if (Double.compare(getDistance(pairQ[0], pairQ[1]), getDistance(pairR[0], pairR[1])) < 0) {
-	    /*	    System.out.println("PairQ: ");
-		    printPointArray(pairQ, "PairQ"); */
-	    return pairQ;
-        } else {
-	    /*	    System.out.println("PairR: ");
-		    printPointArray(pairR, "PairR"); */
-	    return pairR;
-	}
+        else if (Double.compare(getDistance(pairQ[0], pairQ[1]), getDistance(pairR[0], pairR[1])) < 0)
+                return pairQ;
+        else return pairR;
     }
     
     private static double getDistance(Point2D.Double a, Point2D.Double b) {
-	//TODO: Change
-        //double d = Math.hypot(b.getX() - a.getX(), b.getY() - a.getY());
-	return a.distance(b);
+        double d = a.distance(b);
+        return d;
     }
     
-    private static Point2D.Double getMaxXPoint(Point2D.Double[] qx) {
-        Point2D.Double pMax = null;
-        double max = Double.NEGATIVE_INFINITY;
-        for (Point2D.Double p : qx) {
-            if (Double.compare(p.getX(), max) > 0) {
-                pMax = p;
-            }
-        }
-        if(pMax == null) die("pMax == null");
-        return pMax;
+    private static double getL(Point2D.Double[] qx) {
+        return qx[qx.length - 1].x;
     }
     
     public static void parseInput() {
 	Scanner in = new Scanner(System.in);
-//        try {
-//            in = new Scanner(new FileReader("input.txt"));
-//        } catch (FileNotFoundException ex) {
-//            Logger.getLogger(CP.class.getName()).log(Level.SEVERE, null, ex);
-//        }
+        try {
+            in = new Scanner(new FileReader("input.txt"));
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(CP.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
 	while(true) {
 	    if(!in.hasNextLine()) break;
@@ -261,19 +182,14 @@ public class CP {
 	}
 
 	//	Point p = new Point(Double.parseDouble(list.get(1)), Double.parseDouble(list.get(2)), list.get(0));
-	//	Point2D.Double p = new Point2D.Double(Double.valueOf(list.get(1)), Double.valueOf(list.get(2)), list.get(0));
-	//	System.out.println(p.toString());
 	Point2D.Double p = new Point2D.Double(Double.valueOf(list.get(1)), Double.valueOf(list.get(2)));
+	//	System.out.println(p.toString());
 	P[curIndex++] = p;
     }
     
-    private static void printPointArray(Point2D.Double[] arr, String msg) {
-	System.out.println("------------------------------------------");
-	if(msg != null) System.out.println(msg);
-	for(int i = 0; i < arr.length; i++) {
-	    System.out.println(String.format("[%d]: x == %f y == %f", i, arr[i].getX(), arr[i].getY()));
-	}
-	System.out.println("------------------------------------------");	
+    private static void printPointArray(Point2D.Double[] arr) {
+	for(int i = 0; i < arr.length; i++) 
+	    System.out.println(String.format("arr[%d] == %s", i, arr[i].toString()));
     }
     
     private static void die(String msg) {
@@ -283,31 +199,9 @@ public class CP {
     }      
 }
 
-/*class Point {    
-    private final double x;
-    private final double y;
-    private final String name;
-    
-    public Point(double x, double y, String name) {
-        this.x = x;
-        this.y = y;
-        this.name = name;
-    }
-    
-    public double getX() { return x; }
-    public double getY() { return y; }
-    public String getName() { return name; }
-    
-    @Override
-    public String toString() {
-	return String.format("%s (%f, %f)", getName(), getX(), getY());
-    }
-    } */
-
 class PointCmpX implements Comparator<Point2D.Double> {
     @Override
     public int compare(Point2D.Double a, Point2D.Double b) {
-	//	System.out.println(String.format("a %f, b %f", a.getX(), b.getX()));
         return Double.compare(a.getX(), b.getX());
     }
 }
@@ -317,5 +211,5 @@ class PointCmpY implements Comparator<Point2D.Double> {
     public int compare(Point2D.Double a, Point2D.Double b) {
         return Double.compare(a.getY(), b.getY());
     }
-} 
+}
 
