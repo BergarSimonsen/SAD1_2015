7fe8a486b002f7fb4e1a946ff5aa664478866d0c
Bergar Simonsen <bergar.simonsen@gmail.com>
2015-09-16 11:01:49 +0200

    finished parser

--- handin_04/DP.java
+++ handin_04/DP.java
@@ -1,20 +1,65 @@
 import java.util.*;
+import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class DP {
     private static ArrayList<Entity> entities;
     private static Entity[] entArr;
+    private static int[][] cost;
     private static Scanner in;
 
     private static boolean debug = true;
-    
+
+    private static final String number = "\\s*-?[0-9]*";
+    private static final Pattern numberPattern = Pattern.compile(number);
+
     public static void main(String[] args) {
 	parseInput();
+	parseCost();
 	entArr = entities.toArray(new Entity[entities.size()]);
 
-	for(Entity e : entArr) {
-	    log("---------------------------");
-	    log(e.toString());
-	    log("---------------------------");	    
+    }
+
+    private static void parseCost() {
+	try {
+	    cost = new int[24][24];
+
+	    int l = 0;
+	    int k = 0;
+	    
+	    BufferedReader br = new BufferedReader(new FileReader(new File("data/BLOSUM62.txt")));
+	    String line = br.readLine();
+	    while(line != null) {
+		if(!line.startsWith("#")) {
+		    String[] tmp = line.split(" ");
+		    if(l++ > 0) {
+			int j = 0;			
+			for(int i = 0; i < tmp.length; i++) {
+			    if(tmp[i] == null || tmp[i].length() == 0 || tmp[i] == " ")
+				continue;
+			    else if(isNumber(tmp[i])) {
+				if(j < 24 && k < 24) {
+				    String s = tmp[i].trim();
+				    cost[k][j++] = Integer.parseInt(s);
+				}
+			    }
+			}
+			k++;
+		    }
+		}
+		
+		line = br.readLine();
+	    }
+	    br.close();
+	} catch (IOException e) {
+	    e.printStackTrace();
 	}
+    }
+
+    private static boolean isNumber(String s) {
+	Matcher num = numberPattern.matcher(s);
+	return num.matches();
     }
 
     private static void parseInput() {
